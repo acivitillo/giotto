333b4074762e8258bec8ac1cbbeafb3667104392
malinaa96 <katarzyna.malina@te.com>
2021-03-01 16:34:08 +0100

    ♻️ Refactor table controller

--- assets/src/controllers/table_controller.js
+++ assets/src/controllers/table_controller.js
@@ -2,83 +2,49 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
     static targets = ["row"];
-    static values = { entries: String };
+    static values = { maxPageRows: Number };
 
     initialize() {
+        this.startIndex = 0;
+        this.showCurrentPage()
+    };
+
+    firstPage() {
+        this.initialize()
+    }
+
+    previousPage() {
+        if (this.startIndex - this.maxPageRowsValue >= 0) {
+            this.startIndex -= this.maxPageRowsValue
+            this.showCurrentPage()
+        }
+    };
+
+    nextPage() {
+        if (this.startIndex + this.maxPageRowsValue < this.rowTargets.length) {
+            this.startIndex += this.maxPageRowsValue
+            this.showCurrentPage()
+        }
+    };
+
+    lastPage() {
+        this.startIndex = this.rowTargets.length - ((this.rowTargets.length - 1) % this.maxPageRowsValue) - 1
+        this.showCurrentPage()
+    }
+
+    showCurrentPage() {
         this.rowTargets.forEach((element, index) => {
-            if (index < this.entry) {
+            if (index >= this.startIndex & index < this.startIndex + this.maxPageRowsValue) {
                 element.classList.remove("hidden")
             } else {
                 element.classList.add("hidden")
             }
         });
-    };
-
-    hide() {
-        this.rowTargets.forEach(element => {
-            element.classList.add("hidden")
-        });
-    };
-
-    unhide() {
-        this.rowTargets.forEach(element => {
-            element.classList.remove("hidden")
-        });
-    };
-
-    next() {
-        // get first index of next rows
-        var start_index = 0
-        var i;
-        for (i = 0; i < this.rowTargets.length; i++) {
-            if (!this.rowTargets[i].classList.contains("hidden")) {
-                var start_index = i + this.entry;
-                break;
-            }
-        }
-
-        if (start_index < this.rowTargets.length) {
-            // hide previous rows and unhide next rows
-            this.rowTargets.forEach((element, index) => {
-                if (index >= start_index & index < start_index + this.entry) {
-                    element.classList.remove("hidden")
-                } else {
-                    element.classList.add("hidden")
-                }
-            });
-        }
-    };
-
-    prev() {
-        // get first index of prev rows
-        var start_index = 0
-        var i;
-        for (i = 0; i < this.rowTargets.length; i++) {
-            if (!this.rowTargets[i].classList.contains("hidden")) {
-                var start_index = i - this.entry;
-                break;
-            }
-        }
-
-        if (start_index >= 0) {
-            // hide current rows and unhide prev rows
-            this.rowTargets.forEach((element, index) => {
-                if (index >= start_index & index < start_index + this.entry) {
-                    element.classList.remove("hidden")
-                } else {
-                    element.classList.add("hidden")
-                }
-            });
-        }
-
-    };
+    }
 
 
     get rows() {
         return this.rowTarget.value
     };
 
-    get entry() {
-        return parseInt(this.entriesValue)
-    };
 };
