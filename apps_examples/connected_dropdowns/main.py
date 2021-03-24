a0ff4bc79f2308953cd72366edd67f0624fd1775
malinaa96 <katarzyna.malina@te.com>
2021-03-24 12:42:47 +0100

    🚧 Switch from htmx dropdowns to forms

--- apps_examples/connected_dropdowns/main.py
+++ apps_examples/connected_dropdowns/main.py
@@ -1,14 +1,15 @@
 import os
 
 from dominate.tags import div
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from fastapi.responses import HTMLResponse
 
+from giotto.elements import Input, Select
 from giotto.navigation import Sidebar
 from giotto.templates import AppSite
+from giotto.views import FiltersView
 
 from .. import mockapis
-from .views import DropdownView
 
 domain = os.getenv("API_DOMAIN")
 
@@ -24,33 +25,44 @@ def index():
             items=mockapis.sidebar_items, selected={"lev1": "ACOE", "lev2": "Crosstab"}
         ),
     )
-    sources = get_sources()
-    dropdowns = DropdownView(
-        data={"sources": {"options": sources}}, url_prefix=f"{prefix}/dropdowns"
-    )
-    site.content = div(dropdowns.to_tag())
+    dropdowns = generate_filters()
+    site.content = div(dropdowns)
     return site.to_html()
 
 
-@router.get("/dropdowns", response_class=HTMLResponse, tags=["Crosstab"])
-def select_table(source: str, schema: str = None):
+def generate_filters(source: str = "", schema: str = "", table: str = "", message: str = ""):
     sources = get_sources()
-    schemas = get_schemas(source=source)
+    schemas = [] if not source else get_schemas(source=source)
+    tables = [] if not source and not schemas else get_tables(source=source, schema=schema)
 
-    if not schema:
-        data = {
-            "sources": {"options": sources, "selected": source},
-            "schemas": {"options": schemas},
-        }
-    else:
-        tables = get_tables(source=source, schema=schema)
-        data = {
-            "sources": {"options": sources, "selected": source},
-            "schemas": {"options": schemas, "selected": schema},
-            "tables": {"options": tables},
-        }
-    view = DropdownView(data=data, url_prefix=f"{prefix}/dropdowns")
-    return view.to_html()
+    # Cleaning old cached arguments
+    schema = schema if schema in schemas else ""
+    table = table if table in tables else ""
+
+    filters = {
+        "source": Select(options=sources, selected=source),
+        "schema": Select(options=schemas, selected=schema),
+        "table": Select(options=tables, selected=table),
+        "message": Input(placeholder="Add your message", value=message),
+    }
+
+    view = FiltersView(data=filters, url_prefix=f"{prefix}/dropdowns").to_tag()
+    view.add(div(f"input: {source} {schema} {table} {message}"))
+    return view
+
+
+@router.post("/dropdowns", response_class=HTMLResponse, tags=["Crosstab"])
+def select_table(
+    source: str = Form(""),
+    schema: str = Form(""),
+    table: str = Form(""),
+    message: str = Form(""),
+    # refresh: bool = False,
+):
+    # if refresh:
+    #     source, schema, table, message = ("", "", "", "")
+    f = generate_filters(source, schema, table, message)
+    return f.render()
 
 
 def get_sources():
--- apps_examples/connected_dropdowns/views.py
+++ apps_examples/connected_dropdowns/views.py
@@ -1,53 +1,8 @@
-from typing import List
+from typing import List, Dict, Union
 
-from dominate.tags import div
+from dominate.tags import div, input_, form
+from dominate.util import raw
 from pydantic import BaseModel
 
 from giotto.base import Action, BaseView
-from giotto.elements import Row, Row, Select
-
-
-class SourceDropdown(BaseView):
-    def to_tag(self):
-
-        tag = Select(options=self.data)
-        return tag
-
-
-class Dropdown(BaseModel):
-    options: List[str] = []
-    selected: str = ""
-
-
-class DropdownData(BaseModel):
-    sources: Dropdown
-    schemas: Dropdown = Dropdown()
-    tables: Dropdown = Dropdown()
-
-
-class DropdownView(BaseView):
-    data: DropdownData
-
-    def to_tag(self):
-        tag = div(_id="source_dropdowns")
-        sources = Select(
-            **self.data.sources.dict(),
-            name="source",
-            action=Action(
-                get=self.url_prefix,
-                target="#source_dropdowns",
-            ),
-        )
-        schemas = Select(
-            **self.data.schemas.dict(),
-            id_="schemas",
-            name="schema",
-            action=Action(
-                get=(f"{self.url_prefix}?source={self.data.sources.selected}"),
-                target="#source_dropdowns",
-            ),
-        )
-        tables = Select(**self.data.tables.dict(), id_="tables", name="table")
-        row = Row(contents=[sources, schemas, tables])
-        tag.add(row.to_tag())
-        return tag
+from giotto.elements import Row, Row, Select, Input, Button
