ed8e9066443ac6b8fccc0f2a0428807c1a74ac42
malinaa96 <katarzyna.malina@te.com>
2021-03-25 09:09:31 +0100

    🔥 Remove old stuff

--- apps_examples/connected_dropdowns/views.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Dict, Union
-
-from dominate.tags import div, input_, form
-from dominate.util import raw
-from pydantic import BaseModel
-
-from giotto.base import Action, BaseView
-from giotto.elements import Row, Row, Select, Input, Button
