193c9a2a1333def9fc9afcf0b3bd8e456b4ef027
malinaa96 <katarzyna.malina@te.com>
2021-04-06 14:35:26 +0200

    ♻️ Refactor code, add Frame class, remove form_name parameter

--- apps_examples/decorators/lib.py
+++ apps_examples/decorators/lib.py
@@ -1,77 +1,31 @@
 import inspect
 from fastapi import Form, Request
 from fastapi.responses import HTMLResponse
-from typing import List, Dict, Any
-from dominate import tags
+from typing import List, Dict, Any, Callable, Literal
 from dominate.tags import h1, select, option, div, body, head, script, form, button
 from dominate import document
 from pydantic import BaseModel
 from functools import wraps
 
 
+class Frame(BaseModel):
+    id_: str
+    func: Callable
+    target: str = ""
+    arguments: List[str] = []
+    out_target: str = ""
+    tags: List[Any] = []
+    type_: Literal["form", "div"] = "div"
+
+    def to_tag(self):
+        tag = eval(self.type_)
+        tag = tag(self.tags, _id=self.id_)
+        return tag
+
+
 class App(BaseModel):
     app: Any
-    frames: Dict = {}
-    functions: Dict = {}
-
-    def form(self, name):
-        def decorator(func):
-            self.register_url(f"/{name}", func)
-            try:
-                tag = func()
-            except TypeError:
-                tag = func
-            # tag = div(out, _id=name)
-            try:
-                self.frames[name].append(tag)
-            except KeyError:
-                self.frames[name] = []
-                self.frames[name].append(tag)
-            return func
-
-        return decorator
-
-    def append_frame(self, name: str, tag: Any, frame_type=""):
-        try:
-            self.frames[name]["tags"].append(tag)
-        except KeyError:
-            self.frames[name] = {"tags": [], "type": frame_type}
-            self.frames[name]["tags"].append(tag)
-        return self
-
-    def append_function(self, func, route: str = "", target=""):
-        func_name = func.__name__
-        arguments = [arg for arg in inspect.signature(func).parameters]
-        print(arguments)
-        self.functions[func_name] = {
-            "route": route,
-            "func": func,
-            "target": target,
-            "args": arguments,
-        }
-        return self
-
-    async def receiver(self, request: Request, func_name: str):
-        form = await request.form()
-
-        def to_tag():
-            func = self.functions[func_name]["func"]
-            args = self.functions[func_name]["args"]
-            route = self.functions[func_name]["route"]
-            target = self.functions[func_name]["target"]
-            func_args = []
-            for arg in args:
-                try:
-                    func_args.append(form[arg])
-                except KeyError:
-                    raise KeyError(f"Value {arg} in function {func_name} not in form data")
-            tags = func(*func_args)
-            out_tags = self.add_post(route, target, "message", tags)
-            return out_tags
-
-        html_items = [tag.render() for tag in to_tag()]
-        print(html_items)
-        return "\n".join(html_items)
+    frames: Dict[str, Frame] = {}
 
     def register(self):
         self.app.add_api_route(
@@ -80,60 +34,89 @@ class App(BaseModel):
         self.app.add_api_route("/", self.render, methods=["GET"], response_class=HTMLResponse)
         return self
 
-    def add_post(self, hx_post: str, hx_target: str, out_hx_target: str, tags: list):
-        out_tags = []
+    async def receiver(self, request: Request, func_name: str):
+        # How is this request generated ???
+        form = await request.form()
+        print(form)
+
+        def to_tag():
+            from pprint import pprint
+
+            print(func_name)
+            print("\nFRAMES:")
+            pprint({k: v.dict() for k, v in self.frames.items()})
+            frame = self.frames[func_name]
+            func_args = []
+            for arg in frame.arguments:
+                try:
+                    func_args.append(form[arg])
+                except KeyError:
+                    raise KeyError(f"Value {arg} in function {func_name} not in form data")
+            tags = frame.func(*func_args)
+            self.add_post(tags, frame.target, frame.out_target)
+            return tags
+
+        html_items = [tag.render() for tag in to_tag()]
+        # print(html_items)
+        return "\n".join(html_items)
+
+    @staticmethod
+    def add_post(tags: list, hx_target: str, out_hx_target: str):
         for tag in tags:
-            tag["hx-target"] = hx_target
-            tag["hx-post"] = hx_post
-            out_tags.append(tag)
-        btn = button(
-            "Submit",
-            data_hx_post=f"/receiver?func_name={out_hx_target}",
-            data_hx_target=f"#{out_hx_target}",
-        )
-        out_tags.append(btn)
-        return out_tags
+            if hx_target:
+                tag["hx-target"] = f"#{hx_target}"
+                tag["hx-post"] = f"/receiver?func_name={hx_target}"
 
-    def input(self, frame_name: str, target: str = "", out_target: str = ""):
-        def decorator(func):
-            func_name = func.__name__
-            if target != "":
-                _target = f"#{target}"
-                route = f"/receiver?func_name={target}"
-            else:
-                route = f"/receiver?func_name={func_name}"
-
-            tags = func()
-            out_tags = self.add_post(route, _target, out_target, tags)
-            self.append_function(func, route, _target)
-            _ = [self.append_frame(frame_name, tag, "form") for tag in out_tags]
-            return out_tags
-
-        return decorator
-
-    def output(self, frame_name: str):
-        def decorator(func):
-            self.append_function(func)
-            tag = func()
-            self.append_frame(frame_name, tag)
-            return tag
-
-        return decorator
+        if out_hx_target:
+            btn = button(
+                "Submit",
+                data_hx_post=f"/receiver?func_name={out_hx_target}",
+                data_hx_target=f"#{out_hx_target}",
+            )
+            tags.append(btn)
+        return tags
 
     def render(self):
         doc = document()
         _head = head(script(src="https://unpkg.com/htmx.org@1.3.2"))
-        _body = body()
-        for frame in self.frames:
-            if self.frames[frame]["type"] == "form":
-                parent_tag = form(_id=frame)
-            else:
-                parent_tag = div(_id=frame)
-            for element in self.frames[frame]["tags"]:
-                parent_tag.add(element)
-            _body.add(parent_tag)
+        _body = body(*[frame.to_tag() for frame in self.frames.values()])
         doc.add(_head, _body)
         return doc.render()
+
+    def input(self, target: str = "", out_target: str = ""):
+        def decorator(func):
+            tags = func()
+            self.add_post(tags, target, out_target)
+            self.append_frame(func, tags, target, out_target, "form")
+            return tags
+
+        return decorator
+
+    def output(self):
+        def decorator(func):
+            tags = func()
+            self.append_frame(func, tags)
+            return tags
+
+        return decorator
+
+    def append_frame(
+        self, func, tags: Any, target: str = "", out_target: str = "", frame_type: str = "div"
+    ):
+        func_name = func.__name__
+        arguments = [arg for arg in inspect.signature(func).parameters]
+        self.frames[func_name] = Frame(
+            **{
+                "id_": func_name,
+                "func": func,
+                "target": target,
+                "arguments": arguments,
+                "out_target": out_target,
+                "tags": tags,
+                "type_": frame_type,
+            }
+        )
+        return self
 
 
 class Select(BaseModel):
@@ -142,8 +125,8 @@ class Select(BaseModel):
     selected: str = ""
 
     @classmethod
-    def from_list(cls, name: str, options: List):
-        return cls(name=name, options=options)
+    def from_list(cls, name: str, options: List, selected: str = ""):
+        return cls(name=name, options=options, selected=selected)
 
     def to_tag(self):
         sel = select(name=self.name)
--- apps_examples/decorators/main.py
+++ apps_examples/decorators/main.py
@@ -1,63 +1,53 @@
+from dominate.tags import li, p, ul
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from lib import App, Select
-
-from dominate.tags import div, p, ul, li, button
 
 app = FastAPI()
 webapp = App(app=app)
 webapp.register()
 
 
-@webapp.input(
-    frame_name="inp_cascading_selects",
-    target="inp_cascading_selects",
-    out_target="out_cascading_selects",
-)
-def inp_cascading_selects(dd_tables: str = "", dd_fruits: str = "", independents: str = ""):
-    options = ["agrumi", "vegetables", "nuts"]
-    fruits = []
-    independent = ["movies", "books", "etc"]
-    sel1 = Select.from_list("dd_tables", options)
-    if dd_tables != "":
-        fruits = {
-            "agrumi": ["oranges", "lemons"],
-            "vegetables": ["lettuce"],
-            "nuts": ["walnuts", "whatever"],
-        }
-        fruits = fruits[dd_tables]
-        sel1.selected = dd_tables
-    sel1 = sel1.to_tag()
-    sel2 = Select.from_list("dd_fruits", fruits)
-    if dd_fruits != "":
-        sel2.selected = dd_fruits
-    sel2 = sel2.to_tag()
-    sel3 = Select.from_list("independents", independent)
-    if independents != "":
-        sel3.selected = independents
-    sel3 = sel3.to_tag()
+@webapp.input(target="inp_cascading_selects", out_target="out_cascading_selects")
+def inp_cascading_selects(table: str = "", fruit: str = "", independent: str = ""):
+    data = {
+        "agrumi": ["oranges", "lemons"],
+        "vegetables": ["lettuce"],
+        "nuts": ["walnuts", "whatever"],
+    }
+    tables = list(data.keys())
+    fruits = data.get(table, [])
+    independents = ["movies", "books", "etc"]
+
+    sel1 = Select.from_list("table", tables, table).to_tag()
+    sel2 = Select.from_list("fruit", fruits, fruit).to_tag()
+    sel3 = Select.from_list("independent", independents, independent).to_tag()
     return [sel1, sel2, sel3]
 
 
-@webapp.output(frame_name="out_cascading_selects")
-def out_cascading_selects(dd_tables: str = "", dd_fruits: str = "", independents: str = ""):
+@webapp.output()
+def out_cascading_selects(table: str = "", fruit: str = "", independent: str = ""):
     _ul = ul()
-    _ul.add(li(p(f"You selected table: {dd_tables}")))
-    _ul.add(li(p(f"You selected fruit: {dd_fruits}")))
-    _ul.add(li(p(f"You selected independent: {independents}")))
+    _ul.add(li(p(f"You selected table: {table}")))
+    _ul.add(li(p(f"You selected fruit: {fruit}")))
+    _ul.add(li(p(f"You selected independent: {independent}")))
     return [_ul]
 
 
-@webapp.input(frame_name="addtwo", target="out_addtwo")
-def dd_values():
+@webapp.input(out_target="out_addtwo")
+def inp_addtwo():
     vals1 = [1, 2, 3]
     vals2 = [1, 2, 3]
-    sel1 = Select.from_list("vals1", vals1).to_tag()
-    sel2 = Select.from_list("vals2", vals2).to_tag()
+    sel1 = Select.from_list("x", vals1).to_tag()
+    sel2 = Select.from_list("y", vals2).to_tag()
     return [sel1, sel2]
 
 
-@webapp.output(frame_name="out_addtwo")
-def out_addtwo(vals1: int = 0, vals2: int = 0):
-    s = int(vals1) + int(vals2)
+@webapp.output()
+def out_addtwo(x: int = 0, y: int = 0):
+    s = int(x) + int(y)
     return [p(f"The sum of your selected values is {s}")]
+
+
+@webapp.output()
+def hello():
+    return [p("Hello :) This is a static frame!")]
