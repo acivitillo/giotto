9d12f2150aa7fc05a0d453efd72356278ffe1ec5
malinaa96 <katarzyna.malina@te.com>
2021-04-06 18:54:07 +0200

    ⚡️ Switch to giotto components

--- apps_examples/decorators/main.py
+++ apps_examples/decorators/main.py
@@ -1,44 +1,67 @@
 from dominate.tags import li, p, ul
 from fastapi import FastAPI
-from lib import App, Select
+from fastapi.staticfiles import StaticFiles
+
+from giotto.elements import Select, ConnectedDropdowns, Input, Text, Column
+from giotto.navigation import Sidebar
+from giotto.templates import App
+import mockapis
 
 app = FastAPI()
-webapp = App(app=app)
+app.mount("/giotto-statics", StaticFiles(packages=["giotto"]))
+webapp = App(
+    app=app,
+    sidebar=Sidebar(
+        items=mockapis.sidebar_items,
+        selected={"lev1": "ACOE", "lev2": "Scheduler"},
+    ),
+)
 webapp.register()
 
 
-@webapp.input(target="inp_cascading_selects", out_target="out_cascading_selects")
-def inp_cascading_selects(table: str = "", fruit: str = "", independent: str = ""):
+@webapp.input(target="inp_cascading_selects", out_target="out_cascading_selects_1")
+def inp_cascading_selects(table: str = "", fruit: str = "", city: str = "", independent: str = ""):
     data = {
-        "agrumi": ["oranges", "lemons"],
-        "vegetables": ["lettuce"],
-        "nuts": ["walnuts", "whatever"],
+        "table": ["agrumi", "agrumi", "vegetables", "nuts", "nuts"],
+        "fruit": ["oranges", "lemons", "lettuce", "walnuts", "whatever"],
+        "city": ["london", "london", "london", "krakow", "krakow"],
     }
-    tables = list(data.keys())
-    fruits = data.get(table, [])
     independents = ["movies", "books", "etc"]
 
-    sel1 = Select.from_list("table", tables, table).to_tag()
-    sel2 = Select.from_list("fruit", fruits, fruit).to_tag()
-    sel3 = Select.from_list("independent", independents, independent).to_tag()
-    return [sel1, sel2, sel3]
+    sel1 = ConnectedDropdowns(
+        data=data, filters={"table": table, "fruit": fruit, "city": city}
+    ).to_tag()
+    sel3 = Select(name="independent", options=independents, selected=independent).to_tag()
+    return [*sel1, sel3]
+
+
+@webapp.input(out_target="out_cascading_selects_2", class_="flex flex-col m-4 border")
+def out_cascading_selects_1(table: str = "", fruit: str = "", independent: str = ""):
+    text = (
+        f"You selected table: {table}<br>"
+        f"You selected fruit: {fruit}<br>"
+        f"You selected independent: {independent}"
+    )
+    text = Text(value=text).to_tag()
+    inp = Input(placeholder="Add your comment", name="comment").to_tag()
+    return [text, inp]
 
 
 @webapp.output()
-def out_cascading_selects(table: str = "", fruit: str = "", independent: str = ""):
-    _ul = ul()
-    _ul.add(li(p(f"You selected table: {table}")))
-    _ul.add(li(p(f"You selected fruit: {fruit}")))
-    _ul.add(li(p(f"You selected independent: {independent}")))
-    return [_ul]
+def out_cascading_selects_2(comment: str = ""):
+    if comment:
+        tag = p(f'Your comment: "{comment}" has been saved.')
+    else:
+        tag = p()
+    return [tag]
 
 
 @webapp.input(out_target="out_addtwo")
 def inp_addtwo():
     vals1 = [1, 2, 3]
     vals2 = [1, 2, 3]
-    sel1 = Select.from_list("x", vals1).to_tag()
-    sel2 = Select.from_list("y", vals2).to_tag()
+    sel1 = Select(name="x", options=vals1).to_tag()
+    sel2 = Select(name="y", options=vals2).to_tag()
     return [sel1, sel2]
 
 
