74e70eed757b7fa5457e7826cf23d74ac813dc7d
malinaa96 <katarzyna.malina@te.com>
2021-04-09 17:08:44 +0200

    🔥 Remove Transformer

--- giotto/__init__.py
+++ giotto/__init__.py
@@ -2,7 +2,6 @@ from .elements import Box, Button, Input, Select, Table, Text, ClickableIcon, Ro
 from .icons import IconBin, IconDetails, IconPlay, IconStop
 from .navigation import Sidebar
 from .templates import AppSite
-from .transformers import Transformer
 
 
 __version__ = "0.1.0rc0"
