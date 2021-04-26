7fb5e592493f2b788a1efcf4fd5c5b529997b26d
malinaa96 <katarzyna.malina@te.com>
2021-04-26 14:48:39 +0200

    ✅ Add tests for Multiselect and Input

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,4 @@
-from ..giotto.elements import Select
+from ..giotto.elements import Select, MultiSelect, Input
 
 
 def clear_sting(s: str):
@@ -28,5 +28,48 @@ def test_select_with_selected():
         f'<select class="{Select._class}"><option value=""></option>'
         '<option>1</option><option selected="selected">2</option></select>'
     )
+    test_input = partial.render()
+    assert clear_sting(test_input) == clear_sting(expected)
+
+
+def test_multiselect_empty():
+    partial = MultiSelect(options=[])
+    expected = (
+        f'<select class="{Select._class}" multiple="multiple"><option value=""></option></select>'
+    )
+    test_input = partial.render()
+    assert clear_sting(test_input) == clear_sting(expected)
+
+
+def test_multiselect_with_options():
+    partial = MultiSelect(options=["1", "2"])
+    expected = (
+        f'<select class="{Select._class}" multiple="multiple"><option value=""></option>'
+        "<option>1</option><option>2</option></select>"
+    )
+    test_input = partial.render()
+    assert clear_sting(test_input) == clear_sting(expected)
+
+
+def test_multiselect_with_selected():
+    partial = MultiSelect(options=["1", "2"], selected=["1", "2"])
+    expected = (
+        f'<select class="{Select._class}" multiple="multiple"><option value=""></option>'
+        '<option selected="selected">1</option><option selected="selected">2</option></select>'
+    )
+    test_input = partial.render()
+    assert clear_sting(test_input) == clear_sting(expected)
+
+
+def test_input_empty():
+    partial = Input()
+    expected = f'<input class="{partial._class}" placeholder="" type="text">'
+    test_input = partial.render()
+    assert clear_sting(test_input) == clear_sting(expected)
+
+
+def test_input_with_value():
+    partial = Input(value="hello")
+    expected = f'<input class="{partial._class}" placeholder="" type="text" value="hello">'
     test_input = partial.render()
     assert clear_sting(test_input) == clear_sting(expected)
