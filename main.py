22d9f1716c8ad1ef24206615f0bd0353c49f80c1
malinaa96 <katarzyna.malina@te.com>
2021-05-21 17:23:28 +0200

    ⚰️ Remove old Box component

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from apps_examples import mockapis
 from apps_examples.connected_frames import webapp as connected_frames
 from apps_examples.ghpages import webapp as ghpage
 from apps_examples.scheduler import webapp as scheduler
-from giotto.elements import Box, Text
+from giotto.elements import Text
 from giotto.navigation import Sidebar
 from giotto.app import MainApp
 
@@ -13,5 +13,5 @@ webapp = MainApp(sidebar=Sidebar(items=mockapis.sidebar_items), apps=apps)
 @webapp.frame()
 def index():
     text = "### Index page needs improvement."
-    box = Box(contents=[Text(value=text)], centered=True)
+    box = Text(value=text)
     return [box]
